d65c690f664703f7ece78a6093da92fc27a1e23f
Nahuel Mederos <nahuel.mederos@minervafoods.com>
2025-01-28 20:51:59 -0300

    1

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import handlebars from "express-handlebars";
+import __dirname from "./utils.js";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "handlebars");
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
